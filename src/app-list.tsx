85535af1a903751b63831bd91946efeaa59511f3
Rajitha Thilanka <rajithat1998@gmail.com>
2024-05-18 00:24:21 +0530

    first commit

---
+++ src/app-list.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import React from "react";
+
+const AppList = () => {
+  return (
+    <View>
+      <Text>app-list</Text>
+    </View>
+  );
+};
+
+export default AppList;
